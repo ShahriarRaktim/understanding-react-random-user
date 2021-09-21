208dcf0f89776eb83e9f7dfb597a06237408cb90
ShahriarRaktim <shahriarahmedraktim@gmail.com>
2021-09-21 22:47:06 +0600

    add color

--- src/components/Config/Config.js
+++ src/components/Config/Config.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import './Config.css'
 
 const Config = (props) => {
+    const {uuid, username, password} = props.user.login;
     return (
-        <div>
-            <h1>Count in Config.js:{props.steps}</h1>
+        <div className='log-in'>
+            <h1>FOR LOG IN</h1>
+            <h4>UUID:{uuid}</h4>
+            <h4>USERNAME:{username}</h4>
+            <h4>PASSWORD:{password}</h4>
         </div>
     );
 };
--- src/components/Device/Device.js
+++ src/components/Device/Device.js
@@ -2,16 +2,24 @@ import React from 'react';
 import Config from '../Config/Config';
 
 const Device = (props) => {
+    const deviceStyle = {
+        backgroundColor: 'mediumspringgreen',
+        padding: '1px',
+        margin:'5% 25%',
+        borderRadius: '20px'
+    }
     const {gender, name, location, email, picture} = props.user;
     return (
-        <div>
-            <img src={picture.large} alt="" />
+       <div>
+            <div style={deviceStyle}>
+            <img className='rounded-pill mt-2' src={picture.large} alt="" />
             <h2>{gender}</h2>
             <h1>{name.title} {name.first} {name.last}</h1>
             <h3>{email}</h3>
             <h3>{location.city}, {location.country}</h3>
-            
         </div>
+        <Config user={props.user}></Config>
+       </div>
     );
 };
 
